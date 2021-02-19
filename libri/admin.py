cb1f32376fd46615fb7faf142179e1cae79642c2
MrBigSlime <79312910+MrBigSlime@users.noreply.github.com>
2021-02-19 12:26:37 +0100

    Add files via upload

---
+++ libri/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+admin.site.register(Collane)
+admin.site.register(CasaEditrice)
+admin.site.register(TradAutCur)
+admin.site.register(PostfazionePre)
+admin.site.register(Seriale)
+admin.site.register(NonSeriale)
+admin.site.register(SingoliLibri)
+admin.site.register(Utenti)
+admin.site.register(Scaffale)
+admin.site.register(Prestito)
+admin.site.register(Posizione)
