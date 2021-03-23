9fc563e9593461c41cca633906cab30fa15e5aae
MrBigSlime <daniele.teli04@gmail.com>
2021-03-23 10:42:03 +0100

    Test Datalist

--- libri/forms.py
+++ libri/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 from libri.models import *
 
+class Datalist(Select):
+    input_type = 'text'
+    template_name = 'django/forms/widgets/datalist.html'
+    option_template_name = 'django/forms/widgets/datalist_option.html'
+    add_id_index = False
+    checked_attribute = {'selected': True}
+    option_inherits_attrs = False
+
 class InserimentoLibro(forms.Form):
     #Casa Editrice
     NomeCa = forms.CharField(label="Nome CasaEd", max_length=256, widget=forms.TextInput(attrs={'class' : 'formBox', 'placeholder':'Nome Casa Editrice*'}))
